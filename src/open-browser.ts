74c9b30cc50f4f03371f9d1300dc9ad228061ffe
Gunnie <grenneboog@gmail.com>
2017-05-19 09:49:02 -0300

    added refreshWhenReturningToPublication

--- src/open-browser.ts
+++ src/open-browser.ts
@@ -1,24 +1,29 @@
-module dk{
+module dk {
 
-  /**
-    @param result is a json stringified IBasicResponse object
-  */
-  export interface IOpenBrowserRequestSuccessFn {
-      (result: string): null;
-  }
+    /**
+      @param result is a json stringified IBasicResponse object
+    */
+    export interface IOpenBrowserRequestSuccessFn {
+        (result: string): null;
+    }
 
-  /**
-    @param result is a json stringified IBasicResponse object
-  */
-  export interface IOpenBrowserRequestFailureFn {
-      (result: string): null;
-  }
+    /**
+      @param result is a json stringified IBasicResponse object
+    */
+    export interface IOpenBrowserRequestFailureFn {
+        (result: string): null;
+    }
 
-  export interface IOpenBrowserRequest{
-    successCallback: IOpenBrowserRequestSuccessFn;
-    failureCallback: IOpenBrowserRequestFailureFn;
-    url:string;
-    callbackIdentifier?: string;
-  }
+    export interface IOpenBrowserRequest {
+        successCallback: IOpenBrowserRequestSuccessFn;
+        failureCallback: IOpenBrowserRequestFailureFn;
+        url: string;
+        /**
+          Indicates if you want to refresh the previous publication upon exiting/closing the browser,
+          default value is false
+        */
+        refreshWhenReturningToPublication?: boolean;
+        callbackIdentifier?: string;
+    }
 
 }
